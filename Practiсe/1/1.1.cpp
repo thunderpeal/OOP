f8208c674ad3f7c2d367190e97d0d820f88e1c80
thunderpeal <pealsgun@gmail.com>
2019-02-17 22:21:47 +0300

    Create 1.1.cpp

---
+++ Practiсe/1/1.1.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << "Enter your name, please: ";
+	string name = "";
+	cin >> name;
+	cout << "Hello, " << name << "!" << endl;
+	system("pause");
+	return 0;
+}
