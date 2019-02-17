837a95e6ea456f634ace6fbe98dc0a887a7cf35d
thunderpeal <pealsgun@gmail.com>
2019-02-17 22:49:12 +0300

    Create 1.2.cpp

---
+++ Practiсe/1/1.2.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+void summ(float a, float b) { cout << endl << " Сумма этих чисел равна " << a + b << endl; }
+
+void diff(float a, float b) { cout << "Разность этих чисел равна " << a - b << endl; }
+
+void div(float a, float b) {
+	if (b == 0.0) { cout << "Деление на ноль недопустимо!"; }       //вынес все операции в отдельные функции
+	else { cout << "Частное этих чисел равно " << a / b << endl; }
+}
+
+void multi(float a, float b) { cout << "Произведение этих чисел равно " << a * b << endl; }
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b;
+	cout << "Введите два числа: ";
+	cin >> a >> b;
+	summ(a, b);
+	diff(a, b);
+	multi(a, b);
+	div(a, b);
+	system("pause");
+	return 0;
+}
