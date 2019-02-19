a19917bca9dafd22be6fe1819ecd2a56df8859fb
thunderpeal <pealsgun@gmail.com>
2019-02-19 23:32:19 +0300

    -

---
+++ Practiсe/2/2.2.cpp
@@ -0,0 +1,33 @@
+﻿//#include "pch.h"
+#include "math.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	float a = -0.6;
+	float b = 5.3;
+	float t, z = 0.0;
+
+	if (a < b) {
+		z = sqrt(abs(a*a - b * b));
+	}
+	else {
+		z = 1 - 2 * cos(a)*sin(b);
+	}
+
+	if (z < b) {
+		t = pow((z + a * a*b), 1.0 / 3.0);
+	}
+	else if (z == b) {
+		t = 1 - log10(z) + cos(a*a*b);
+	}
+	else {
+		t = 1 / cos(z*a);
+	}
+	cout << "a = " << a << endl << "b = " << b << endl;
+	cout << "z = " << z << endl << "t = " << t << endl;
+	system("pause");
+	return 0;
+}
+
---
+++ Practiсe/2/2.3.cpp
@@ -0,0 +1,33 @@
+﻿//#include "pch.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	float a = 4.6;
+	float b = 1.5;
+	float t, y = 0;
+
+	if (a <= b) {
+		y = ((a - b) / (a + b)) * ((a + b) / (a*a - a * b + b * b));
+	}
+	else {
+		y = a + log(b*b);
+	}
+
+	if (y == b) {
+		t = (2 * y + sqrt(y*y - a)) / (2 * b - sqrt(a*a - y));
+	}
+	else if (y < b) {
+		t = pow(sin(y), 2) + 1 / tan(a - b);
+	}
+	else {
+		t = -pow(abs(y*sin(a)), 1.0 / 3.0) +1 / sqrt(y*cos(b));
+	}
+	cout << "a = " << a << endl << "b = " << b << endl;
+	cout << "y = " << y << endl << "t = " << t << endl;
+	system("pause");
+	return 0;
+}
+
