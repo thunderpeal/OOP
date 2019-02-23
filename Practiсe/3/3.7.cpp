02233e561394ed720a04be0c2136de31ea789a5c
thunderpeal <pealsgun@gmail.com>
2019-02-23 15:24:38 +0300

    Create 3.7.cpp

---
+++ Practiсe/3/3.7.cpp
@@ -0,0 +1,23 @@
+﻿//#include "pch.h"
+#include <iostream>
+#include "3.3.cpp"
+using namespace std;
+
+struct library {
+	string name;
+	string city;
+	int year;
+	Book books[100];
+};
+
+int main(int argc, char const *argv[])		//для корректной работы необходимо закомментить main в 3.3.cpp
+{
+	library library1 = { "The Russian State Library", "Moscow", 1862, {
+		{"The Master and Margarita", "Mikhail Bulgakov", 1967, 500, true}
+	} 
+	};
+	library1.books[0].print_name();
+	system("pause");
+	return 0;
+}
+
