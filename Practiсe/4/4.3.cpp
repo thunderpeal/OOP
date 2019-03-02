52b6b8c09088e11bcaa3e2c6025eacc44866cb9b
thunderpeal <pealsgun@gmail.com>
2019-03-02 23:53:15 +0300

    -

---
+++ Practiсe/4/4.3.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+int **set(int **a, int m, int n) { //создание двумерного массива 
+	for (int i = m; i > 0; i--) {
+		a[i] = new int[n];
+		for (int j = n; j > 0; j--) {
+			a[i][j] = rand() % 41 + 10;
+		}
+	}
+	return a;
+}
+
+int **display(int **a, int m, int n) { //вывод двумерного массива на экран
+	for (int i = m; i > 0; i--) {
+		for (int j = n; j > 0; j--) {
+			cout << a[i][j]<<" ";
+		}
+		cout << endl;
+	}
+	return a;
+}
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(0));
+	int rows;
+	int columns;
+	
+	cout << "Введите количество строк, затем столбцов двумерного массива: ";
+	cin >> rows >> columns;
+	int ** a = new int*[rows];
+	a = set(a, rows, columns);
+	a = display(a, rows, columns);
+
+	system("pause");
+	return 0;
+}
---
+++ Practiсe/6/6.1.cpp
@@ -0,0 +1,56 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+class Property{
+	protected:
+		double worth;
+	public:
+		Property() {}
+		virtual double result() = 0;
+	
+};
+class Appartment : public Property {
+	public:
+		Appartment(double a) { worth = a; }
+		double result() {
+			return worth / 1000;
+		}
+};
+
+class Car : public Property {
+	public:
+		Car (double a) { worth = a; }
+		double result() {
+			return worth / 500;
+		}
+
+};
+
+class CountryHouse : public Property {
+	public:
+		CountryHouse(double a) { worth = a; }
+		double result() {
+			return worth / 1000;
+		}
+};
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	Property **a = new Property*[7];
+	a[0] = new Appartment(1000000);
+	a[1] = new Appartment(500000);
+	a[2] = new Appartment(100000);
+	a[3] = new Car(20000.5);
+	a[4] = new Car(2000.12);
+	a[5] = new CountryHouse(1000000);
+	a[6] = new CountryHouse(5000000);
+	for (int i = 0; i < 7; i++) {
+		cout << "Налог на данное имущество равен: " << a[i]->result() << endl;
+	}
+	for (int i = 0; i < 7; i++) delete a[i];
+	delete[] a;
+	system("pause");
+	return 0;
+}
