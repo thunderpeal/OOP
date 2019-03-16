2537182df2c01fb050c6acb343341de48510ecda
thunderpeal <pealsgun@gmail.com>
2019-03-17 01:47:21 +0300

    Update 7.2.cpp

--- Practiсe/7/7.2.cpp
+++ Practiсe/7/7.2.cpp
@@ -4,44 +4,43 @@
 using namespace std;
 
 class Figure {
-	protected:
-		int diag_1;
-		int	diag_2;
-		int angle;
-	public:
-		virtual double find_s() = 0;
+protected:
+	int diag_1;
+	int	diag_2;
+	int angle;
+public:
+	virtual void formula() = 0;
 };
 
-class Quadrilateral : Figure {
-	public:
-		Quadrilateral(int d1, int d2, int a) {
-			diag_1 = d1;
-			diag_2 = d2;
-			angle = a;
-		}
-		double find_s() {
-			return 0.5*(diag_1 * diag_2*sin(angle));
-		}
+class Quadrilateral : public Figure {
+public:
+	virtual double find_s() = 0;
 };
 
-class Rhomb : Figure {
-	public:
-		Rhomb(int d1, int d2) {
-			diag_1 = d1;
-			diag_2 = d2;
-		}
-		double find_s() {
-			return 0.5*(diag_1 * diag_2);
-		}
+class Rhomb : public Quadrilateral {
+public:
+	Rhomb(int d1, int d2) {
+		diag_1 = d1;
+		diag_2 = d2;
+	}
+	double find_s() {
+		return 0.5*(diag_1 * diag_2);
+	}
+	void formula() {
+		cout << "0.5*(diag_1 * diag_2)" << endl;
+	}
 };
 
-class Rectangle : Figure {
-	public:
-		Rectangle(int d, int a) {
-			diag_1, diag_2 = d;
-			angle = a;
-		}
-		double find_s() {
-			return 0.5*(diag_1 * diag_2*sin(angle));
-		}
+class Rectangle : public Quadrilateral {
+public:
+	Rectangle(int d, int a) {
+		diag_1, diag_2 = d;
+		angle = a;
+	}
+	double find_s() {
+		return 0.5*(diag_1 * diag_2*sin(angle));
+	}
+	void formula() {
+		cout << "0.5*(diag^2)" << endl;
+	}
 };
