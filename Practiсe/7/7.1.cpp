c224c5d3057a763496df7248e825154b4d7f043d
thunderpeal <pealsgun@gmail.com>
2019-03-17 01:05:34 +0300

    added

---
+++ Practiсe/7/7.1.cpp
@@ -0,0 +1,50 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+class Animal {
+	protected:
+		string name;
+		int age;
+	public:
+		void set_name(string s) {
+			name = s;
+		}
+		void set_age(int a) {
+			age = a;
+		}
+		virtual void say_smth() = 0;
+};
+
+class Cat : Animal {
+	string breed;
+	public:
+		void set_breed(string b) {
+			breed = b;
+		}
+		void say_smth() {
+			cout << "Mrrr";
+		}
+};
+
+class Dog : Animal {
+	public:
+		Dog() {
+			cout << "Bark!";
+		}
+		void say_smth() {
+			cout << "sorry for barking, dude";
+		}
+};
+
+class Parrot : Animal {
+	string color;
+	public:
+		Parrot(int a, string s) {
+			color = s;
+			age = a;
+		}
+		void say_smth() {
+			cout << "Popka Durak!";
+		}
+};
---
+++ Practiсe/7/7.2.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Figure {
+	protected:
+		int diag_1;
+		int	diag_2;
+		int angle;
+	public:
+		virtual double find_s() = 0;
+};
+
+class Quadrilateral : Figure {
+	public:
+		Quadrilateral(int d1, int d2, int a) {
+			diag_1 = d1;
+			diag_2 = d2;
+			angle = a;
+		}
+		double find_s() {
+			return 0.5*(diag_1 * diag_2*sin(angle));
+		}
+};
+
+class Rhomb : Figure {
+	public:
+		Rhomb(int d1, int d2) {
+			diag_1 = d1;
+			diag_2 = d2;
+		}
+		double find_s() {
+			return 0.5*(diag_1 * diag_2);
+		}
+};
+
+class Rectangle : Figure {
+	public:
+		Rectangle(int d, int a) {
+			diag_1, diag_2 = d;
+			angle = a;
+		}
+		double find_s() {
+			return 0.5*(diag_1 * diag_2*sin(angle));
+		}
+};
