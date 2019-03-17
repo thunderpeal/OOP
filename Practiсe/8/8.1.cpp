3e59fb2ac06e6ca82da1f76fd048195cfa096b7a
thunderpeal <pealsgun@gmail.com>
2019-03-17 15:00:58 +0300

    Create 8.1.cpp

---
+++ Practiсe/8/8.1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	ifstream file("file.txt");
+	if (file.is_open()) {
+		string line = "";
+		while (!file.eof()) {
+			file >> line;
+			cout << line << endl;
+		}
+		file.close();
+	}
+	else {
+		cout << "Невозможно открыть файл!";
+	}
+	system("pause");
+	return 0;
+}
