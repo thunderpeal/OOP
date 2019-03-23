306e9fdf2d12963d3f1ff241da06371851f3f6fe
thunderpeal <pealsgun@gmail.com>
2019-03-23 23:24:33 +0300

    laba 1.2

---
+++ Labs/1/main_2.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "Complex.h"
+
+int main(int argc, char const *argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	ifstream file("complex.txt");
+
+	if (file.is_open()) {
+		int n = 0;
+		file >> n;
+		Complex* p = new Complex[n];
+
+		for (int i = 0; i < n; i++) {
+			file >> p[i].Rez;
+			file >> p[i].Imz;
+		}
+		file.close();
+
+		int index = -1;
+		double max = 0;
+		Complex m;
+		for (int i = 0; i < n; i++) {
+			if (abs_value(p[i]) > max) {
+				m = p[i];
+				max = abs_value(p[i]);
+				index = i;
+			}
+		}
+
+		cout << "Максимальный модуль имеет комплексное число под номером (начиная с 1) " << index + 1 << endl;
+		cout << "Модуль равен " << max << endl << endl;
+		delete[] p;
+		p = NULL;
+	}
+	else {
+		cout << "Нет данных!" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
