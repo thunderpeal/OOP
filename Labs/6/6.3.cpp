5fb3fc661cab5df136f3882170cb558310f42c4c
thunderpeal <pealsgun@gmail.com>
2019-04-27 23:25:32 +0300

    Create 6.3.cpp

---
+++ Labs/6/6.3.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal {
+protected:
+	bool is_predator;
+	int age;
+	int tame_0_to_10;
+public:
+	Animal(bool i_p, int a, int t) {
+		is_predator = i_p;
+		age = a;
+		tame_0_to_10 = t;
+	}
+	virtual void sound() = 0;
+};
+
+class Dog : public Animal {
+public:
+	string breed;
+	Dog(bool is_p, int a, int t, string b) : Animal(is_p, a, t) {
+		breed = b;
+	}
+
+	void sound() {
+		cout << "Woof!" << endl;
+	}
+};
+
+class Cat : public Animal {
+public:
+	bool is_big_cat;
+	Cat(bool is_p, int a, int t, string b, bool is_b_c) : Animal(is_p, a, t) {
+		is_big_cat = is_b_c;
+	}
+
+	void sound() {
+		cout << "Meow!" << endl;
+	}
+};
