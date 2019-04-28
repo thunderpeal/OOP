6937ef3e89d10c4fffbff6bbcc6ee008e03c431c
thunderpeal <pealsgun@gmail.com>
2019-04-28 15:05:07 +0300

    5

---
+++ Labs/5/5.1.cpp
@@ -0,0 +1,59 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Complex {
+private:
+	double re;
+	double img;
+public:
+	Complex(double r, double i) {
+		re = r;
+		img = i;
+	};
+
+	Complex& operator= (Complex& value) {
+		this->img = value.img;
+		this->re = value.re;
+		return *this;
+	};
+
+	Complex& operator++ () {
+		this->re += 1;
+		this->img += 1;
+		return *this;
+	};
+
+	Complex& operator-- () {
+		this->re -= 1;
+		this->img -= 1;
+		return *this;
+	};
+
+	friend bool operator== (const Complex& left, const Complex& right) {
+		bool is_real = left.re == right.re;
+		bool is_image = left.img == right.img;
+		return is_real && is_image;
+	};
+
+	friend ostream& operator<<(ostream &out, const Complex& value) {
+		out << "К.ч. = " << value.re << " + i * " << value.img << endl;
+		return out;
+	};
+
+	friend istream& operator>>(istream &in, const Complex &number) {
+		in >> number.re >> number.img;
+		return in;
+	};
+
+	double module() {
+		return sqrt(pow(re, 2) + pow(img, 2));
+	};
+
+	void display() {
+		cout << "Действительная часть = " << re << endl;
+		cout << "Мнимая часть = " << img << endl;
+		cout << "Модуль к. ч. = " << this->get_abs() << endl << endl; //ссылка на данный объект
+	};
+};
---
+++ Labs/5/5.2.cpp
@@ -0,0 +1,55 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Vector {
+private:
+	double x;
+	double y;
+public:
+	Vector(double a, double b) {
+		x = a;
+		y = b;
+	}
+
+	Vector& operator= (Vector& value) {
+		this->x = value.x;
+		this->y = value.y;
+		return *this;
+	}
+
+	friend bool operator== (const Vector& left, const Vector& right) {
+		bool is_x = left.x == right.x;
+		bool is_y = left.y == right.y;
+		return is_x && is_y;
+	}
+
+	friend ostream& operator<< (ostream& out, const Vector& value) {
+		out << "( " << value.x << " , " << value.y << " )" << endl;
+		return out;
+	}
+
+	friend istream& operator>> (istream& is, const Vector& value) {
+		is >> value.x >> value.y;
+		return is;
+	}
+
+	float module() {
+		return sqrt(pow(x, 2) + pow(y, 2));
+	}
+
+	void summ(Vector b) {
+		x += b.x;
+		y += b.y;
+	}
+
+	void diff(Vector b) {
+		x -= b.x;
+		y -= b.y;
+	}
+
+	void display() {
+		cout << x << " " << y << endl;
+	}
+};
