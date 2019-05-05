9e6ae9a6e52fdcbbc15b50619d96d9b77ef60b86
Vyacheslav <pealsgun@gmail.com>
2019-05-05 22:39:17 +0300

    Update 3.3.cpp

--- Labs/3/3.3.cpp
+++ Labs/3/3.3.cpp
@@ -1,6 +1,3 @@
-﻿// ConsoleApplication29.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include "pch.h"
 #include <iostream>
 #include <cmath>
