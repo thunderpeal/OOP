179f63f6b0c45a5ea59a5d5efba0281f55481af2
thunderpeal <pealsgun@gmail.com>
2019-05-05 23:00:30 +0300

    -

---
+++ Labs/8/8.1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "6.1.cpp"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<Student> vec1;
+	cout << "Total count of students is: ";
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string name, surname, group;
+		int age;
+		cout << "Enter your name, surname, age and group: ";
+		cin >> name >> surname >> age >> group;
+		Student student(name, surname, age, group);
+		vec1.push_back(student);
+	};
+	system("pause");
+	return 0;
+}
---
+++ Labs/8/8.2.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "5.1.cpp"
+#include <list>
+using namespace std;
+
+int main()
+{
+	list<Complex> com;
+	int n;
+	cout << "Amount of numbers: ";
+	cin >> n;
+	for (int i = 0; i < n; i++){
+		double re, img;
+		cout << "Enter real and image parts: ";
+		cin >> re >> img;
+		Complex complex(re, img);
+		com.push_back(complex);
+	}
+	system("pause");
+	return 0;
+}
